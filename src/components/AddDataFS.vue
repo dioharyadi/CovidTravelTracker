23add4a3739700bbc6ede63bd09b85bc3e96f568
Melvin Dio Haryadi <dio.haryadiii@gmail.com>
2022-04-05 22:10:19 +0800

    edited AddDataFS

--- src/components/AddDataFS.vue
+++ src/components/AddDataFS.vue
@@ -4,8 +4,8 @@
 <script>
 import axios from 'axios'
 import firebaseApp from '../firebase.js';
-import {getFirestore} from "firebase/firestore";
-import {doc, setDoc} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 const db = getFirestore(firebaseApp);
 
 
@@ -15,6 +15,7 @@ export default {
     data() {
         return  {
             AU : '',
+            AU_nc : {},
             CN : '',
             ID : '',
             JP : '',
@@ -27,26 +28,38 @@ export default {
         async  fetchCovidCase() {
             console.log("Working")
             var enddateObj = new Date()
-            var startdateObj = new Date(new Date().setDate(enddateObj.getDate()-30));
+            enddateObj.setHours(8,0,0,0)
+            var startdateObj = new Date(new Date().setDate(enddateObj.getDate()-30))
+            startdateObj.setHours(8,0,0,0)
             var enddate = enddateObj.toISOString();
             var startdate = startdateObj.toISOString();
-            enddate = enddate.substring(0, enddate.indexOf('T')+1);
-            startdate = startdate.substring(0, startdate.indexOf('T')+1);
-            enddate += "00:00:00Z"
-            startdate += "00:00:00Z"
             console.log(startdate, enddate)
             await axios.get('https://api.covid19api.com/total/country/' + 'AU' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.AU = response))
             this.AU.data.forEach(data => setDoc(doc(db, "Australia", data["Date"]), {
                 Country: "Australia", date: data["Date"], Cases: data["Cases"]
             }))
+            // const start = startdateObj;
+            // const end = enddateObj;
+            // let loop = new Date(start);
+            // while (loop <= end) {
+            //     console.log(loop.toISOString())
+            //     var loopISO = loop.toISOString()
+            //     this.AU_nc[loopISO] = 0
+            //     let newDate = loop.setDate(loop.getDate() + 1);
+            //     loop = new Date(newDate);
+            // }
+            // this.AU_nc[startdate] = this.AU.data[1]['Cases'] - this.AU.data[0]['Cases']
+            // this.AU_nc[enddate] = this.AU.data[30]['Cases'] - this.AU.data[29]['Cases']
+            // console.log(this.AU_nc)
             await axios.get('https://api.covid19api.com/total/country/' + 'CN' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.CN = response))
             this.CN.data.forEach(data => setDoc(doc(db, "China", data["Date"]), {
                 Country: "China", date: data["Date"], Cases: data["Cases"]
             }))
+
             await axios.get('https://api.covid19api.com/total/country/' + 'ID' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.ID = response))
             this.ID.data.forEach(data => setDoc(doc(db, "Indonesia", data["Date"]), {
                 Country: "Indonesia", date: data["Date"], Cases: data["Cases"]
-            }))            
+            }))      
             await axios.get('https://api.covid19api.com/total/country/' + 'JP' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.JP = response))
             this.JP.data.forEach(data => setDoc(doc(db, "Japan", data["Date"]), {
                 Country: "Japan", date: data["Date"], Cases: data["Cases"]
